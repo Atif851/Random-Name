9572166eb443e23681bfa94263a1fff754ebf0d1
Atif851 <154266128+Atif851@users.noreply.github.com>
2024-07-07 18:44:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+var text=document.getElementById("text");
+
+
+function A() {
+    var arrA = ["AQIL","AQIB","ATIF"]
+    var random = Math.floor(Math.random()*3)
+    text.innerText = arrA[random]
+    
+}
+ function B() {
+var arrA = ["BASIT","BILAL","BAKHTAWER"]
+var random = Math.floor(Math.random()*3)
+    text.innerText = arrA[random]
+     }
+ function C() {
+     var arrA = ["CAT","CELL","CARBON"]
+   var random = Math.floor(Math.random()*3)
+   text.innerText = arrA[random]
+  
+ }function D() {
+    var arrA = ["DEFENDER","DANISH","DADA"]
+     var random = Math.floor(Math.random()*3)
+    text.innerText = arrA[random]
+    
+ }
+ function E() {
+     var arrA = ["ELECTRON","ELEVEN","ELL"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+function F() {
+    var arrA = ["FISH","FARHAN","FAISAL"]
+    var random = Math.floor(Math.random()*3)
+    text.innerText = arrA[random] 
+}
+ function G() {
+     var arrA = ["GULL","GUN","GERMANY"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function H() {
+     var arrA = ["HASAN","HUSAIN","HANAN"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function I() {
+     var arrA = ["IQBAL","IRFAN","ISPR"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function J() {
+     var arrA = ["JUNAID","JAMSHAID","JUGG"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function K() {
+     var arrA = ["KASHIF","KARAN","KAMAL"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function L() {
+     var arrA = ["LITHIUM","LOL","LAST"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function M() {
+     var arrA = ["MUSTAFA","MUDASIR","MEHMOOD"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function N() {
+     var arrA = ["NASIR","NEUTRON","NASA"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function O() {
+     var arrA = ["OOFFF","ORANGE","OXYGEN"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function P() {
+     var arrA = ["PAKISTAN","PAT","PAD"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function Q() {
+     var arrA = ["QUESTION","QUALITY","QUANTITY"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function R() {
+     var arrA = ["RANDOM","ROMAN","RUN"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function S() {
+     var arrA = ["SHAN","SFER","SYBER"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function T() {
+     var arrA = ["TANVEER","TRUK","TRAIN"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function U() {
+     var arrA = ["UMAR","UMAIR","URANIUM"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+ function V() {
+     var arrA = ["VERY","VERYGOOD","VERYBAD"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function W() {
+     var arrA = ["WISH","WOW","WELL"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function X() {
+     var arrA = ["XBOX","XYLOPHONE","X-RAY"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function Y() {
+     var arrA = ["YAHAYA","YOU","YOUNG"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }function Z() {
+     var arrA = ["ZAIN","ZINC","ZAHOOR"]
+     var random = Math.floor(Math.random()*3)
+     text.innerText = arrA[random]  
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var btn2 = document.getElementById("check");
+
+// btn2.addEventListener("click",function () {
+//     btn2.innerHTML.style.background="green"
+    
+// })
+
+
+
+
